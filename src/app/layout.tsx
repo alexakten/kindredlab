e1bfdd73714356114e709bac44082143ecced9c8
Alex <alex.akten@outlook.com>
2023-11-29 23:07:51 +1100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Kindred Lab",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
