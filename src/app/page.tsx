1e74f5ab3b566120545b89bc31a089eb6fa53d5b
Alex <alex.akten@outlook.com>
2023-11-29 17:34:57 +1100

    button swedish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
               className="rounded-full bg-white px-4 py-2 text-black"
               type="button"
             >
-              Book a call -{">"}
+              {lang === "en" ? "Book a call" : "Boka samtal"} -{">"}
             </button>
           </div>
         </div>
