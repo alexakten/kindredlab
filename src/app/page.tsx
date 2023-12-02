ccd675a22fca08f272530a7a310321d10017b2cd
Alex <alex.akten@outlook.com>
2023-12-02 15:49:40 +1100

    w-full testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -649,7 +649,7 @@ export default function Home() {
               <div className="xs:flex-row xs:items-center flex flex-col items-end justify-between gap-4 pt-12">
                 {/* Name and Title */}
                 <div>
-                  <div className="flex flex-row items-center gap-4">
+                  <div className="flex w-full flex-row items-center gap-4">
                     {/* Image */}
                     <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full">
                       <Image
