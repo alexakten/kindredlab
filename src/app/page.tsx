d875e4c2797c37a665c6dbb2756fc5689ba1bb11
Alex <alex.akten@outlook.com>
2024-06-28 19:28:17 +0100

    mt mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       </section>
 
       {/* Hero */}
-      <section className="z-10 mt-28 flex flex-col items-center text-center sm:mt-48">
+      <section className="z-10 mt-20 flex flex-col items-center text-center sm:mt-48">
         <h1 className="mt-4 max-w-2xl font text-center text-[clamp(32px,5.5vw,60px)] font-semibold leading-[1.15] tracking-tighter">
           We help startups raise <br /> their next round
         </h1>
