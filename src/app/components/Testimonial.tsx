6613d059996bbeca59751809e2cfb4b7fe9e3cac
Alex <alex.akten@outlook.com>
2024-07-10 08:20:11 +0200

    color testimonials

--- src/app/components/Testimonial.tsx
+++ src/app/components/Testimonial.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Testimonial({ imageUrl, text, user, role }: { imageUrl: string, text: string, user: string, role: string }) {
   return (
-    <div className="mx-2 w-72 sm:w-96 h-full flex flex-col justify-between rounded-lg border border-gray-200 shadow-sm bg-gray-50 p-6">
+    <div className="mx-2 w-72 sm:w-96 h-full flex flex-col justify-between rounded-lg border border-gray-200 shadow-sm bg-white p-6">
       <p>{text}</p>
       <div className="mt-4 flex items-center gap-2">
         {/* <div className="w-10 h-10 relative bg-gray-200 rounded-full overflow-hidden">
