2dfe2a68eff71e58e47b28c24118cae46dbd1bd2
Alex <alex.akten@outlook.com>
2024-07-10 12:54:24 +0200

    icons

--- src/app/Testimonials.tsx
+++ src/app/Testimonials.tsx
@@ -3,43 +3,43 @@ export const testimonials = [
     text: "Working with the team is my favorite part of the week.",
     user: "Arvid Winterfeld",
     role: "CEO at Qura",
-    imageUrl: ""
+    imageUrl: "/images/profiles/arvid.png"
   },
-  {
-    text: "They truly understand great design.",
-    user: "Alex Wikström",
-    role: "CEO at Darwin",
-    imageUrl: ""
-  },
+  // {
+  //   text: "They truly understand great design.",
+  //   user: "Alex Wikström",
+  //   role: "CEO at Darwin",
+  //   imageUrl: ""
+  // },
   {
     text: "Professional team doing a really good job. After the first job I hired them again.",
     user: "Jan Eriksson",
-    role: "CEO at Flexiwaggon",
-    imageUrl: ""
+    role: "CEO at Flexiwaggon & TTS",
+    imageUrl: "/images/profiles/jan.png"
   },
   {
     text: "We are grateful for having worked with them, and hope to do it again in the future.",
     user: "Magnus Ivarsson",
     role: "CEO at MycoMine",
-    imageUrl: ""
+    imageUrl: "/images/profiles/magnus.png"
   },
   {
     text: "They really understood our company and our vision. I can recommend these guys.",
     user: "Hans Delking",
     role: "CEO at Payable",
-    imageUrl: ""
+    imageUrl: "/images/profiles/hans.png"
   },
-  {
-    text: "My startup coach said my website looked like some million-dollar business.",
-    user: "Alex Aktén",
-    role: "CEO at Airbon",
-    imageUrl: ""
-  },
+  // {
+  //   text: "My startup coach said my website looked like some million-dollar business.",
+  //   user: "Alex Aktén",
+  //   role: "CEO at Airbon",
+  //   imageUrl: "/images/profiles/alex-a.png"
+  // },
   {
     text: "Really happy working with them.",
     user: "Karthik Laxman",
-    role: "CEO at Stockholm Water Tech",
-    imageUrl: ""
+    role: "CEO at SWT",
+    imageUrl: "/images/profiles/karthik.png"
   },
 ];
 
--- src/app/components/Testimonial.tsx
+++ src/app/components/Testimonial.tsx
@@ -5,7 +5,7 @@ export default function Testimonial({ imageUrl, text, user, role }: { imageUrl: 
     <div className="mx-2 w-72 sm:w-96 h-full flex flex-col justify-between rounded-lg border border-gray-200 shadow-sm bg-white p-6">
       <p>{text}</p>
       <div className="mt-4 flex items-center gap-2">
-        {/* <div className="w-10 h-10 relative bg-gray-200 rounded-full overflow-hidden">
+        <div className="w-10 h-10 relative bg-gray-200 rounded-full overflow-hidden">
 
           <Image
             src={imageUrl}
@@ -14,7 +14,7 @@ export default function Testimonial({ imageUrl, text, user, role }: { imageUrl: 
             objectFit="cover"
           />
 
-        </div> */}
+        </div>
         <div>
           <p className="font-medium">{user}</p>
           <p className="text-xs opacity-50">{role}</p>
