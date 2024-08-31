c3f6af22275631fa982d39df5a1c4983b60f67f0
Alex <alex.akten@outlook.com>
2024-08-31 12:37:50 +0200

    hero

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,51 +2,76 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex w-full items-center justify-start pt-8">
-      <Link href={"/"} className="z-10 flex items-center gap-1">
+    // <nav className="mt-8 max-w-2xl rounded-full flex w-full items-center justify-between bg-gray-100 px-8 py-3">
+    //   <Link href={"/"} className="z-10 flex items-center gap-1">
+    //     <svg
+    //       width="14"
+    //       height="30"
+    //       viewBox="0 0 133 183"
+    //       fill="none"
+    //       xmlns="http://www.w3.org/2000/svg"
+    //     >
+    //       <path
+    //         d="M79.4453 52.6755L79.4453 0.853027L53.1617 0.853026L53.1617 52.6755L79.4453 52.6755Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         d="M105.758 78.9778L105.758 52.6753L79.4553 52.6753L79.4553 78.9778L105.758 78.9778Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         d="M105.742 182.62L105.742 130.799L79.4586 130.799L79.4586 182.62L105.742 182.62Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         d="M132.062 182.616L132.062 156.313L0.663742 156.313L0.663741 182.616L132.062 182.616Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         d="M132.031 130.796L132.031 78.9731L105.748 78.9731L105.748 130.796L132.031 130.796Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         d="M79.4219 130.796L79.4219 104.493L26.8479 104.493L26.8479 130.796L79.4219 130.796Z"
+    //         fill="black"
+    //       />
+    //       <path
+    //         fill-rule="evenodd"
+    //         clip-rule="evenodd"
+    //         d="M105.75 52.6743L132.04 78.9647V78.9731L105.75 78.9731V52.6743Z"
+    //         fill="black"
+    //       />
+    //     </svg>
+    //     <p className="text-xs font-[450] leading-[0.95] tracking-tight mix-blend-difference ">
+    //       Kindred <br />
+    //       Laboratory
+    //     </p>
+    //   </Link>
+    //   <Link href="mailto:hello@kindredlab.io" className="text-sm font-medium">Say hello</Link>
+    // </nav>
+    <nav className="top-0 z-20 flex w-full max-w-8xl items-center justify-center pt-8 sm:px-10 sm:pt-12">
+      <div className="flex flex-col items-center justify-center gap-1">
         <svg
-          width="19"
-          height="30"
-          viewBox="0 0 133 183"
+          width="28"
+          height="28"
+          viewBox="0 0 40 40"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M79.4453 52.6755L79.4453 0.853027L53.1617 0.853026L53.1617 52.6755L79.4453 52.6755Z"
-            fill="black"
-          />
-          <path
-            d="M105.758 78.9778L105.758 52.6753L79.4553 52.6753L79.4553 78.9778L105.758 78.9778Z"
-            fill="black"
-          />
-          <path
-            d="M105.742 182.62L105.742 130.799L79.4586 130.799L79.4586 182.62L105.742 182.62Z"
-            fill="black"
-          />
-          <path
-            d="M132.062 182.616L132.062 156.313L0.663742 156.313L0.663741 182.616L132.062 182.616Z"
-            fill="black"
-          />
-          <path
-            d="M132.031 130.796L132.031 78.9731L105.748 78.9731L105.748 130.796L132.031 130.796Z"
-            fill="black"
-          />
-          <path
-            d="M79.4219 130.796L79.4219 104.493L26.8479 104.493L26.8479 130.796L79.4219 130.796Z"
-            fill="black"
-          />
-          <path
             fill-rule="evenodd"
             clip-rule="evenodd"
-            d="M105.75 52.6743L132.04 78.9647V78.9731L105.75 78.9731V52.6743Z"
+            d="M0 0H24V8V16H16V24H24V16H32H40V40H0V0ZM40 8V0H32V8H40Z"
             fill="black"
           />
         </svg>
-        <p className="text-base font-[450] leading-[0.95] tracking-tight mix-blend-difference ">
-          Kindred <br />
-          Laboratory
+
+        <p className="relative font-freight text-xl font-semibold leading-[1] tracking-tight">
+          first
+          <span className="font-medium italic">house</span>
+          {/* <span className="absolute top-2 font-normal -right-1 font-inter text-[0.3rem]">®</span> */}
         </p>
-      </Link>
+      </div>
     </nav>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import Link from "next/link";
 import Navbar from "./components/Navbar";
-import Testimonial from "./components/Testimonial";
-import Pricing from "./components/Pricing";
-import Project from "./components/Project";
 import Footer from "./components/Footer";
-import Marquee from "./components/Marquee";
 import { Motion } from "./components/Motion";
-import ProjectScroll from "./components/ProjectScroll";
-import ProjectCard from "./components/ProjectCard";
-import { useRef } from "react";
+import Logos from "./components/Logos";
 
 export default function Home() {
   return (
@@ -17,49 +11,39 @@ export default function Home() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ ease: "easeInOut", duration: 0.5 }}
-        className="flex w-full max-w-xl flex-col items-center"
+        className="flex w-full max-w-8xl flex-col items-center"
       >
         <Navbar />
         {/* Hero */}
-        <section className="z-10 mt-20 flex w-full flex-col items-start text-left">
-          <h1 className="text-3xl font-normal leading-[1.4] tracking-tight">
-            We&apos;re a brand, web and product design studio{" "}
-            <span className="opacity-50">
-              for startups that iterate fast.
-              {/* — all with one simple monthly fee. */}
-            </span>
-            <br />
-            <br />
-            Make any design request{" "}
-            <span className="opacity-50">
-              — we&apos;ll deliver it to you by the end of the week.
-            </span>
-            <br />
-            <br />
-            <span className="opacity-50">There are currently </span>
-            2 spots available.
-            <br />
-            <br />
-            <span className="opacity-50">Our pricing starts at </span>
-            80,000 SEK per month.{" "}
+        <section className="z-10 mt-24 flex w-full max-w-2xl flex-col items-center text-center">
+          <h1 className="text-7xl font-medium leading-[1] tracking-tight">
+            Web and product <span className="font-freight italic text-[80px]">design studio</span>{" "}
           </h1>
-          <div className="mt-12 flex items-center gap-3 tracking-normal">
+          <h2 className="mt-6 max-w-xl text-xl opacity-50">
+            We&apos;re a fully remote studio designing landing pages and
+            products for startups that iterate fast.
+          </h2>
+          <div className="mt-8 flex items-center gap-3 tracking-normal">
             <Link
               href={"mailto:hello@kindredlab.io"}
-              className="rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium shadow-sm  hover:bg-gray-50 hover:shadow-none"
+              className="rounded-full bg-black text-white px-5 py-2.5 text-sm font-medium hover:bg-zinc-800"
             >
               Book a call
             </Link>
 
             <Link
               href={"/work"}
-              className="group flex items-center gap-1 rounded-full border border-white bg-white px-5 py-2.5 text-sm font-medium hover:border-gray-200  hover:bg-gray-50"
+              className="group flex items-center gap-1 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium   hover:bg-gray-50"
             >
               See work{" "}
               <span className="inline-block translate-x-[1px] tracking-normal transition-transform duration-200 group-hover:translate-x-1">
                 -&gt;
               </span>
             </Link>
+          </div>
+          <p className="mt-24 text-sm opacity-50">Trusted by teams at</p>
+          <div className="mt-6">
+            <Logos />
           </div>
         </section>
 
