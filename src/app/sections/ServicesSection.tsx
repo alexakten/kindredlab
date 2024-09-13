6716a56599d03f2a496cf1380b259a270e195c75
Alex <alex.akten@outlook.com>
2024-09-13 18:31:52 +0200

    how it works

--- src/app/sections/ServicesSection.tsx
+++ src/app/sections/ServicesSection.tsx
@@ -9,7 +9,10 @@ import {
   Cursor,
   CubeFocus,
   DeviceMobileCamera,
+  ArrowBendRightUp,
 } from "@phosphor-icons/react/dist/ssr";
+
+import Image from "next/image";
 
 const services = [
   {
@@ -73,7 +76,7 @@ export default function ServicesSection() {
           );
         })}
       </div>
-      <div className="mt-24 grid w-full max-w-6xl grid-cols-2 gap-4 px-4 text-left">
+      <div className="mt-24 grid w-full max-w-6xl grid-cols-2 gap-8 px-4 text-left">
         <div className="flex flex-col rounded-2xl border border-white border-opacity-40 bg-white bg-opacity-10 p-8">
           <p className="text-xs">Step 1</p>
           <h3 className="mt-2 text-2xl font-medium">Make any design request</h3>
@@ -163,9 +166,75 @@ export default function ServicesSection() {
             Implement your designs and test
           </h3>
           <p className="mt-2 opacity-75">
-            From the ground up, we refresh all your visuals — from logo to font
-            and colors.
+            See how our designs perform in real life.
           </p>
+          <div className="relative mt-8 border-t border-white border-opacity-10 px-12 pb-12 pt-8">
+            <div className="absolute bottom-20 right-2 z-50 flex gap-1">
+              <NavigationArrow
+                className="duocolor-pink"
+                weight="duotone"
+                fill="white"
+              />
+              <div className="rounded-xs mt-3 bg-pink-600 px-1 text-xs font-semibold leading-[1.4]">
+                Marketing Team
+              </div>
+            </div>
+            {/* <div className="flex flex-col gap-2">
+              <div className="relative flex aspect-[16/9] w-full -rotate-0 flex-col items-center justify-center rounded-xl border border-white border-opacity-40 bg-white bg-opacity-10 p-2 text-center">
+                <p className="text-[12px] font-medium  leading-[1.1]">
+                  Supercharge your <br />{" "}
+                  <span className="font-freight  text-[13px] italic">
+                    design & marketing
+                  </span>
+                </p>
+                <p className="mt-1 max-w-[4rem] text-[4px] opacity-75">
+                  We're a fully remote design partner for next-gen businesses
+                  that iterate fast.
+                </p>
+              </div>
+              <div className="mt-3 flex flex-wrap gap-x-2 gap-y-2">
+                <div className="rotate-4 rounded-full border border-white border-opacity-40 bg-white bg-opacity-10 px-2 py-1">
+                  <p className="text-sm opacity-75">+4% conversion</p>
+                </div>
+                <div className="rotate-4 rounded-full border border-white border-opacity-40 bg-white bg-opacity-10 px-2 py-1">
+                  <p className="text-sm opacity-75">+7 sign ups</p>
+                </div>
+                <div className="rotate-4 rounded-full border border-white border-opacity-40 bg-white bg-opacity-10 px-2 py-1">
+                  <p className="text-sm opacity-75">+16 new leads</p>
+                </div>
+              </div>
+            </div> */}
+
+            <div className="relative flex flex-col gap-2">
+              <div className="flex aspect-[16/9] w-full -rotate-0 flex-col items-center justify-center rounded-xl border border-white border-opacity-0 bg-white bg-opacity-0 p-2 text-center">
+                <div className="absolute -left-8 top-4 -rotate-[3deg] rounded-full border border-white border-opacity-40 bg-white bg-opacity-20 px-2 py-1 text-white">
+                  <p className="text-sm opacity-100">+12% conversion</p>
+                </div>
+                <div className="absolute -right-8 scale-105 top-0 rotate-[4deg] rounded-full border border-white border-opacity-40 bg-white bg-opacity-20 px-2 py-1 text-white">
+                  <p className="text-sm opacity-100">+24 sign ups</p>
+                </div>
+                <div className="absolute right-12 top-20 rotate-[2deg] rounded-full border border-white border-opacity-40 bg-white bg-opacity-20 scale-150 px-2 py-1 text-white">
+                  <p className="text-sm opacity-100">+$10,000 revenue</p>
+                </div>
+                <div className="absolute -bottom-4 right-16 -rotate-[2deg] rounded-full border border-white border-opacity-40 bg-white scale-110 bg-opacity-20 px-2 py-1 text-white">
+                  <p className="text-sm opacity-100">+87 new leads</p>
+                </div>
+                <div className="absolute -left-8 scale-125 bottom-10 rotate-[6deg] rounded-full border border-white border-opacity-40 bg-white bg-opacity-20 px-2 py-1 text-white">
+                  <p className="text-sm opacity-100">98% retention rate</p>
+                </div>
+                {/* <p className="text-[12px] font-medium  leading-[1.1]">
+                  Supercharge your <br />{" "}
+                  <span className="font-freight  text-[13px] italic">
+                    design & marketing
+                  </span>
+                </p>
+                <p className="mt-1 max-w-[4rem] text-[4px] opacity-75">
+                  We're a fully remote design partner for next-gen businesses
+                  that iterate fast.
+                </p> */}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="flex flex-col rounded-2xl border border-white border-opacity-40 bg-white bg-opacity-10 p-8">
           <p className="text-xs">Step 4</p>
@@ -173,9 +242,31 @@ export default function ServicesSection() {
             Iterate on user feedback
           </h3>
           <p className="mt-2 opacity-75">
-            From the ground up, we refresh all your visuals — from logo to font
-            and colors.
+            Gather feedback and we iterate on our design.
           </p>
+          <div className="relative mt-8 border-t border-white border-opacity-10 px-8 pb-12 pt-8">
+            <div className="mx-2 flex flex-col justify-between rounded-2xl border border-white border-opacity-20 bg-white bg-opacity-10 p-6 shadow-sm ">
+              <div className="flex items-center gap-2">
+                <div className="relative flex h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-purple-200">
+                  <p className="text-lg">🦄</p>
+                </div>
+                <div className="">
+                  <p className="font-medium">User</p>
+                  <p className="text-xs tracking-normal opacity-50">4h ago</p>
+                </div>
+              </div>
+              <p className="mt-5">
+                The app looks fantastic — so much better than before! Would it
+                be possible to add a back button to the navigation?{" "}
+              </p>
+              <div className="mt-5 flex items-center justify-between rounded-lg border-white border-opacity-40 bg-white bg-opacity-20 px-2 py-2">
+                <p>Thanks for your feedback! | </p>
+                <div className="flex h-6 w-8 items-center justify-center rounded-full border border-white border-opacity-40 bg-white bg-opacity-30">
+                  <ArrowBendRightUp />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
