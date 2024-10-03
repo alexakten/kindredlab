efc64b98586cad31068ee0a012dda2d3542e036e
Alex <alex.akten@outlook.com>
2024-10-03 15:56:48 +0200

    cards height

--- src/app/sections/ServicesSection.tsx
+++ src/app/sections/ServicesSection.tsx
@@ -119,16 +119,16 @@ export default function ServicesSection() {
         </div>
 
         {/* Design */}
-        <div className="relative flex min-w-[20rem] flex-col overflow-hidden rounded-3xl border border-white border-opacity-40 bg-zinc-100 px-4 py-6 backdrop-blur-sm sm:min-w-[36rem] md:p-8">
+        <div className="relative flex min-w-[20rem] flex-col overflow-hidden rounded-3xl border border-white border-opacity-40 bg-zinc-100 px-4 py-6 sm:min-w-[36rem] md:p-8">
           <Image
             src="/images/design.webp"
             alt="Websites"
             width={100}
             height={100}
-            className="pointer-events-none absolute left-0 top-0 -z-10 h-full w-full scale-150 object-cover blur-lg sm:scale-125"
+            className="pointer-events-none absolute left-0 top-0 h-full w-full scale-150 object-cover blur-lg sm:scale-125"
           />
           <p className="font-basier text-xs opacity-50">DESIGN SUBSCRIPTION</p>
-          <h3 className="mt-6 text-3xl font-semibold sm:mt-12 sm:text-6xl">
+          <h3 className="z-10 mt-6 text-3xl font-semibold sm:mt-12 sm:text-6xl">
             Flexible design subscription
           </h3>
           <p className="mt-4 max-w-sm opacity-50">
@@ -183,24 +183,56 @@ export default function ServicesSection() {
         </div>
 
         {/* Content */}
-        <div className="relative flex min-w-[20rem] flex-col overflow-hidden rounded-3xl border border-white border-opacity-40 bg-zinc-100 px-4 py-6 backdrop-blur-sm sm:min-w-[36rem] md:p-8">
+        <div className="relative flex min-w-[20rem] flex-col overflow-hidden rounded-3xl border border-white border-opacity-40 bg-zinc-100 px-4 py-6 sm:min-w-[36rem] md:p-8">
           <Image
             src="/images/content.webp"
-            alt="Content"
+            alt="Websites"
             width={100}
             height={100}
-            className="pointer-events-none absolute left-0 top-0 -z-10 h-full w-full scale-150 object-cover blur-lg sm:scale-125"
+            className="pointer-events-none absolute left-0 top-0 h-full w-full scale-150 object-cover blur-lg sm:scale-125"
           />
-          <p className="font-basier text-xs opacity-50">DESIGN SUBSCRIPTION</p>
-          <h3 className="mt-6 text-3xl font-semibold sm:mt-12 sm:text-6xl">
+          <p className="font-basier text-xs opacity-50">CONTENT</p>
+          <h3 className="z-10 mt-6 text-3xl font-semibold sm:mt-12 sm:text-6xl">
             Content to elevate your marketing
           </h3>
           <p className="mt-4 max-w-sm opacity-50">
             Refresh your website and branding. We build stunning websites in
             Webflow that showcase your products.
           </p>
-          <div className="mb-0 h-40 w-full sm:mt-12 sm:h-64"></div>
-          <div className="mt-16 flex w-full items-center justify-between">
+          <div className="mt-6 h-40 w-full sm:mt-12 sm:h-72 ">
+            <div className="relative mt-0 grid grid-cols-1 gap-4 pb-28 text-xs md:grid-cols-2">
+              <div className="absolute bottom-24 right-24 flex gap-1">
+                <NavigationArrow
+                  // className="duocolor-purple"
+                  weight="fill"
+                  fill="#fff"
+                />
+                <div className="z-50 mt-3 rounded-sm bg-white px-1 text-xs font-medium leading-[1.4] text-black">
+                  You
+                </div>
+              </div>
+
+              <div className="text-b flex flex-col gap-2">
+                <p className="font-medium">To do</p>
+                <div className="max-w-[240px] rotate-0 rounded-md border border-white bg-white bg-opacity-10 px-4 py-2 shadow-sm">
+                  Ads for campaign
+                </div>
+                <div className="max-w-[240px] rotate-0 rounded-md border border-white bg-white bg-opacity-10 px-4 py-2 shadow-sm">
+                  New UI for search
+                </div>
+                <div className="absolute bottom-12 left-14 w-full max-w-[220px] rotate-6 rounded-md border border-white bg-white  bg-opacity-10 px-4 py-2 shadow-sm sm:bottom-8 sm:max-w-[240px] md:left-32">
+                  Design new website
+                </div>
+              </div>
+              <div className="hidden flex-col gap-2 md:flex">
+                <p className="font-medium">Done</p>
+                <div className="max-w-[240px] rotate-0 rounded-md border border-white bg-white bg-opacity-10 px-4 py-2 shadow-sm">
+                  Create new logo
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 flex w-full items-center justify-between sm:mt-16">
             <p className="hidden font-basier text-xs opacity-50 sm:block">
               BOOK A FREE DEMO{" "}
             </p>
