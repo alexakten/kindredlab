ae899c4044cb9e86ca1986f2294564d608b2a63d
Alex <alex.akten@outlook.com>
2024-12-10 22:19:52 +0100

    two images

--- src/app/[lang]/components/HeroScroll.tsx
+++ src/app/[lang]/components/HeroScroll.tsx
@@ -23,7 +23,7 @@ export default function HeroScroll() {
 
   // Array of image objects with orientation and source
   const images = [
-    // { orientation: "landscape", src: "/images/hero-scroll/hero-scroll-2.webp" },
+    { orientation: "landscape", src: "/images/hero-scroll/hero-scroll-2.webp" },
     // { orientation: "portrait", src: "/images/hero-scroll/hero-scroll-3.webp" },
     // { orientation: "square", src: "/images/hero-scroll/hero-scroll-4.webp" },
     { orientation: "landscape", src: "/images/hero-scroll/qura-website.png" },
