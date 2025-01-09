7b72d289146d17c1aab2e35f72152ac4989af1d1
Alex <alex.akten@outlook.com>
2025-01-09 20:23:29 +0100

    hero image

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -59,7 +59,7 @@ export default async function Home({
         {/* Hero */}
         <section className="flex min-h-[100svh] w-full max-w-8xl flex-col items-center overflow-hidden bg-black">
           <div className="rounded-m relative h-full w-full overflow-hidden bg-zinc-950 py-20 pt-28 sm:pt-36">
-            {/* <Image
+            <Image
               src="/images/hero-green.png"
               alt="Hero image"
               width={1000}
@@ -67,7 +67,7 @@ export default async function Home({
               className="pointer-events-none absolute left-0 top-0 h-full w-full blur-xl brightness-75 sm:w-full sm:scale-105 sm:object-cover"
               priority
               loading="eager"
-            /> */}
+            />
             {/* <Motion
               initial={{ opacity: 0, y: -20, filter: "blur(1rem)" }}
               animate={{ opacity: 1, y: 0, filter: "blur(0)" }}
