876481c4032c7afe648981b207d8cc62407cc5d4
Alex <alex.akten@outlook.com>
2025-01-10 12:05:39 +0100

    hero on full screen

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -57,17 +57,17 @@ export default async function Home({
       >
         <Navbar lang={lang} dict={dict} />
         {/* Hero */}
-        <section className="flex min-h-[100svh] w-full max-w-8xl flex-col items-center overflow-hidden bg-black">
-          <div className="rounded-m relative h-full w-full overflow-hidden bg-zinc-950 py-20 pt-28 sm:pt-36">
-            <Image
-              src="/images/hero-green.png"
-              alt="Hero image"
-              width={1000}
-              height={1000}
-              className="pointer-events-none absolute left-0 top-0 h-full w-full blur-xl brightness-75 sm:w-full sm:scale-105 sm:object-cover"
-              priority
-              loading="eager"
-            />
+        <section className="max-w- flex min-h-[100svh] w-full flex-col items-center overflow-hidden bg-black">
+          <Image
+            src="/images/hero-green.png"
+            alt="Hero image"
+            width={1000}
+            height={1000}
+            className="pointer-events-none absolute left-0 top-0 h-full w-full blur-xl brightness-75 sm:w-full sm:scale-105 sm:object-cover"
+            priority
+            loading="eager"
+          />
+          <div className="rounded-m relative h-full w-full flex flex-col items-center overflow-hidden py-20 pt-28 sm:pt-36">
             {/* <Motion
               initial={{ opacity: 0, y: -20, filter: "blur(1rem)" }}
               animate={{ opacity: 1, y: 0, filter: "blur(0)" }}
