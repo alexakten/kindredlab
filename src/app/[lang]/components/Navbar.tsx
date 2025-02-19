1284fecca509d4179a53d01014cefb0ad2b9cc67
Alex <alex.akten@outlook.com>
2025-02-19 13:46:46 +0100

    font sabon change?

--- src/app/[lang]/components/Navbar.tsx
+++ src/app/[lang]/components/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar({ lang, dict }: NavbarProps) {
                 className="z-10 flex items-center gap-1.5"
               >
                 <div className="flex items-center justify-center gap-1">
-                  <p className="relative font-freight text-xl font-medium leading-[0.95] tracking-[-0.000rem]">
+                  <p className="relative font-sabon text-xl font-medium leading-[0.95] tracking-[-0.000rem]">
                     kindred
                     {/* <span className="font-normal italic">house</span> */}
                   </p>
