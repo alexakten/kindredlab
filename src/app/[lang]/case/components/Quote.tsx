d63b8a3e5c0cb700150aefbfd1c0257e082e8566
Alex <alex.akten@outlook.com>
2025-03-07 15:55:24 +0100

    apos

--- src/app/[lang]/case/components/Quote.tsx
+++ src/app/[lang]/case/components/Quote.tsx
@@ -10,7 +10,7 @@ export default function Quote({
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h2 className="font-minion font-regular max-w-4xl text-center text-6xl leading-[1.2] tracking-tight">
-        "{quote}"
+        &apos;{quote}&apos;
       </h2>
       <p className="mt-4 text-center text-sm opacity-100">
         {author}
