459d017ebca2d612e9314bc4a1fa5f4d2923c24c
Alex <alex.akten@outlook.com>
2025-03-08 09:38:36 +0100

    padding hero

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -67,7 +67,7 @@ export default async function Home({
             priority
             loading="eager"
           /> */}
-          <div className="rounded-m relative flex h-full w-full flex-col items-center overflow-hidden py-20 pt-12 sm:pt-28">
+          <div className="rounded-m relative flex h-full w-full flex-col items-center overflow-hidden pb-20 pt-12 sm:pt-28">
             <Motion
               initial={{ opacity: 0, y: -20, filter: "blur(1rem)" }}
               animate={{ opacity: 1, y: 0, filter: "blur(0)" }}
