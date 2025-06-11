68303c9ac34603a744818649bf517e98c4e7b400
Alex <alex.akten@outlook.com>
2025-06-11 11:23:29 +0200

    favicon location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/zhy0giy.css" />
-        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
       </head>
       <body>
         <Analytics />
